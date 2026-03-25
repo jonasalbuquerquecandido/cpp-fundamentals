e87317a6971d81645f8a275a38f4df01e1153c2e
Jonas Albuquerque Cândido <jonasalbuquerque.c@gmail.com>
2026-03-25 18:05:58 -0300

    feat: add basic sintax

---
+++ 01-basics/examples/input_output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    int numero;
+
+    cout << "Digite um número: ";
+    cin >> numero;
+
+    cout << "Você digitou: " << numero << endl;
+    return 0;
+}
---
+++ 01-basics/examples/variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int idade = 25;
+    float altura = 1.75;
+    char letra = 'a';
+    string nome = "jonas";
+    bool ativo = true;
+
+    cout << "idade: " << idade << endl;
+    cout << "altura: " << altura << endl;
+    cout << "nome: " << letra << endl;
+    cout << "ativo: " << ativo << endl;
+    cout << "nome: " << nome << endl;
+     
+    return 0;
+  }
