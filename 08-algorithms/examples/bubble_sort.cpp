4f11a1be529895d2fbd711ce5207b659836917ee
Jonas Albuquerque Cândido <jonasalbuquerque.c@gmail.com>
2026-03-27 10:39:20 -0300

    feat: i finished the last examples for  sorting the array elements.

--- 08-algorithms/examples/bubble_sort.cpp
+++ 08-algorithms/examples/bubble_sort.cpp
@@ -4,9 +4,24 @@
 using namespace std;
 
 int main () {
-    int arr[5] = {5, 3, 2, 4, 1};
+    int arr[5] = {5, 3, 2, 4, 1}; // Criação do array
+    bool swapped;
 
-    for (int i = 0; i < 5; i++){
-
+    for (int i = 0; i < 5; i++){  // Criação do loop do array i para em cada posição até menor que 5 // controla a passagem pelo array
+        swapped = false;
+        for (int j = 0; j < 4; j++) { // Criação de uma segunda variável j  // deve percorrer o array interno e trocar os elementos
+            if (arr[j] > arr[j + 1]) { // Se a variável j é maior é j mair 1 entao:
+                int temp = arr[j]; // Criação da variável temp interia que recebe arr j
+                arr[j] = arr[j + 1]; //arr j recbe array j mais 
+                arr[j + 1] = temp; // arrya j + 1 recebe temp
+                swapped = true;
+            }
+        }
+        if (!swapped) break;
     }
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
 }
